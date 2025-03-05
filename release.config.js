0ca4febf79f88404bbc7897dbd6693280311d5f0
simochee <lollipop@simochee.net>
2025-03-05 23:59:22 +0900

    chore: release workflow

---
+++ release.config.js
@@ -0,0 +1,32 @@
+const WXT_SUBMIT_CMD = String.raw`
+  pnpm wxt submit \
+    --chrome-zip ./.output/*-chrome.zip \
+    --firefox-zip ./.output/*-firefox.zip \
+    --firefox-source-zip ./.output/*-sources.zip
+`;
+
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+	branches: ["main"],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		["@semantic-release/npm", { npmPublish: false }],
+		// [
+		//   '@semantic-release/exec',
+		//   { prepareCmd: WXT_SUBMIT_CMD }
+		// ],
+		[
+			"@semantic-release/github",
+			{
+				assets: [
+					{ path: "./.output/*-chrome.zip" },
+					{ path: "./.output/*-firefox.zip" },
+				],
+				labels: ["release"],
+			},
+		],
+		"@semantic-release/git",
+	],
+};
